ec80c8775b05189a72d9c21bf97996f1a3842d62
Mao Yanbin <mybhenry@gmail.com>
2017-11-26 15:09:25 +0800

    changes for AndOrTest.

--- src/AndOrTest.java
+++ src/AndOrTest.java
@@ -3,8 +3,12 @@
  */
 public class AndOrTest {
     public static void main(String[] args) {
+	// some comment
+	//some more comment
+	//some other comment
         System.out.println(1&4);
         System.out.println(1|4);
 
     }
 }
+//test line - channnnged
